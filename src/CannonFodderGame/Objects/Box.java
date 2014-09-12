207b596988d97c5187993352e164fcb48ecde618
Tómas <theodortomastheodorsson@gmail.com>
2014-09-12 08:22:58 +0000

    Ready to draw boxes and lines

--- src/CannonFodderGame/Objects/Box.java
+++ src/CannonFodderGame/Objects/Box.java
@@ -16,27 +16,31 @@ public class Box extends Shape{
 	private Line lefLine;
 	
 	public Box(){
+		//Can be used to draw a rectangle.
 		this.setShapeType(org.lwjgl.opengl.GL11.GL_QUADS);
 		
-		System.out.println("hello");
+		//This sets the bottom line of the box.
 		this.botLine = new Line();
 		this.botLine.setX1(Gdx.input.getX());
 		this.botLine.setY1(Gdx.graphics.getHeight() -  Gdx.input.getY());
 		this.botLine.setX2(Gdx.input.getX());
 		this.botLine.setY2(Gdx.graphics.getHeight() -  Gdx.input.getY());
-	
+		
+		//This sets the right line of the box
 		this.rigLine = new Line();
 		this.rigLine.setX1(Gdx.input.getX());
 		this.rigLine.setY1(Gdx.graphics.getHeight() -  Gdx.input.getY());
 		this.rigLine.setX2(Gdx.input.getX());
 		this.rigLine.setY2(Gdx.graphics.getHeight() -  Gdx.input.getY());
 		
+		//this sets the top line of the box.
 		this.topLine = new Line();
 		this.topLine.setX1(Gdx.input.getX());
 		this.topLine.setY1(Gdx.graphics.getHeight() -  Gdx.input.getY());
 		this.topLine.setX2(Gdx.input.getX());
 		this.topLine.setY2(Gdx.graphics.getHeight() -  Gdx.input.getY());
 		
+		//This sets the left line of the box
 		this.lefLine = new Line();
 		this.lefLine.setX1(Gdx.input.getX());
 		this.lefLine.setY1(Gdx.input.getY());
@@ -44,6 +48,7 @@ public class Box extends Shape{
 		this.lefLine.setY2(Gdx.input.getY());
 		
 	}
+	//Getters for each side of the box.
 	public Line getBotLine(){return this.botLine;}
 	public Line getLefLine(){return this.lefLine;}
 	public Line getTopLine(){return this.topLine;}
